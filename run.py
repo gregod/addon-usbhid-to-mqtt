bba517b3893ed52fab8c67b18981f6374418d675
Gregor Godbersen <git@doamo.de>
2021-04-22 19:30:07 +0200

    Allow non-numeric codes
    Supports chars without modifier keys based on scancode to US keyboard
    layout mapping.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import asyncio
-from evdev import InputDevice, ecodes,list_devices
+from evdev import InputDevice, ecodes,list_devices, util
 import signal, sys
 import json
 import requests
@@ -74,17 +74,30 @@ async def listener(dev):
         logging.info("Token: {}".format(token))
 
 
+scancodes = {
+    # Scancode: ASCIICode
+    # https://stackoverflow.com/a/19757397
+    0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
+    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'q', 17: u'w', 18: u'e', 19: u'r',
+    20: u't', 21: u'y', 22: u'u', 23: u'i', 24: u'o', 25: u'p', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
+    30: u'a', 31: u's', 32: u'd', 33: u'f', 34: u'g', 35: u'h', 36: u'j', 37: u'k', 38: u'l', 39: u';',
+    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'z', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
+    50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+}
+
+
+
 async def wait_for_token(dev):
     buffer=""
     async for event in dev.async_read_loop():
+
          if (event.type == ecodes.EV_KEY and event.value == 1):
-            if event.code == 28: # enter was pressed
+            parsed_event = util.categorize(event)
+            if parsed_event.scancode == 28: # enter was pressed
                 yield buffer
                 buffer=""
-            elif event.code >= 2 and event.code <= 10: # keys 1-9
-                buffer += str(event.code - 1)
-            elif event.code == 11: # key 0
-                buffer += "0"
+            elif parsed_event.scancode in scancodes:
+                buffer += scancodes[parsed_event.scancode]
 
             if len(buffer) > int(config["max_token_length"]): # protect buffer len
                 buffer = ""
